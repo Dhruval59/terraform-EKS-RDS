1c62e2c214b0c96f5807a6e59da5213c3b409383
Dhruval Sharma <43564061+Dhruval59@users.noreply.github.com>
2025-08-28 18:41:49 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -53,7 +53,7 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_agent_policy" {
 
 resource "aws_iam_role_policy_attachment" "autoscaling_policy" {
   role       = aws_iam_role.eks_worker_nodes.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonScalingFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
